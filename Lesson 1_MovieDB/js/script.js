2928f38509e117ae9da57d3d3eb0cbcb3c43b4bc
Darya <darya.drazdouskaya@gmail.com>
2020-07-31 12:02:28 +0300

    Add if-else statements and for loop

--- Lesson 1_MovieDB/js/script.js
+++ Lesson 1_MovieDB/js/script.js
@@ -12,12 +12,30 @@ const personalMovieDB = {
   private: false
 };
 
-const answerLastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
-      answerRateFilm1 = prompt('На сколько оцените его?', ''),
-      answerLastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
-      answerRateFilm2 = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[answerLastFilm1] = answerRateFilm1;      // Записали ответы в объект movies в формате объекта
-personalMovieDB.movies[answerLastFilm2] = answerRateFilm2;
+// Цикл автоматизирует задавание вопросов пользователю
+for (let i = 0; i < 2; i++) {
+  const answerLastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+        answerRateFilm = prompt('На сколько оцените его?', '');
+  
+  if (answerLastFilm != null && answerRateFilm != null &&       // нажата ли кнопка отмена
+      answerLastFilm != '' && answerRateFilm != '' && 
+      answerLastFilm.length < 50 ) {
+        personalMovieDB.movies[answerLastFilm] = answerRateFilm;  // Записали ответы в объект movies в формате объекта
+        console.log('Успех!');
+      } else {
+        console.log('Провал!');
+        i--;
+      }
+}
 
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
